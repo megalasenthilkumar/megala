d0a8778e711a46751ba6c3672994baeec16cc920
megalasenthilkumar <61401678+megalasenthilkumar@users.noreply.github.com>
2020-03-16 11:51:00 +0530

    Add files via upload

---
+++ sum of digits.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	   long n,sum;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a number ");
+n=sc.nextLong();
+for(sum=0 ;n!=0 ;n/=10)
+{
+sum+=n%10;
+}
+System.out.println("Sum of digits "+sum);
+	}
+}
